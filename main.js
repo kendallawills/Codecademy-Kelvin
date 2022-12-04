958170bf5bdf31b09f6c75d907d750e0dd3969b6
Kendall <kendallawills@gmail.com>
2022-12-04 16:14:12 -0500

    Add Newton

--- main.js
+++ main.js
@@ -8,3 +8,10 @@ let fahrenheit = celsius * (9/5) + 32;
 fahrenheit = Math.floor(fahrenheit);
 
 console.log(`The temperature is ${fahrenheit} degrees in Fahrenheit.`);
+
+// Newton
+let newton = celsius * (33/100);
+//Newton Round Number
+newton = Math.floor(newton);
+
+console.log(`The temperature is ${newton} degrees in Newton.`);
